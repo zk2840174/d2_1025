d21d811190e338a5fa3e478533666d504fba37c6
zk202308A <zk202308A@gmail.com>
2024-10-25 11:23:37 +0900

    1st commit

--- src/main/java/org/zerock/d2/controller/SampleController.java
+++ src/main/java/org/zerock/d2/controller/SampleController.java
@@ -1,4 +1,24 @@
 package org.zerock.d2.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/sample")
+@RequiredArgsConstructor
 public class SampleController {
+
+    @GetMapping("hello")
+    public String[] hello() {
+
+        log.info("--------------------------");
+        log.info("hello");
+
+        return new String[] {"Hello", "World"};
+    }
 }
