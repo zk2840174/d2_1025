aab433f12f50e4c045ba09ddc263f863e24f3b7c
zk202308A <zk202308A@gmail.com>
2024-10-25 11:25:25 +0900

    2nd commit

--- src/main/java/org/zerock/d2/controller/SampleController.java
+++ src/main/java/org/zerock/d2/controller/SampleController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SampleController {
 
+
+
+
     @GetMapping("hello")
     public String[] hello() {
 
